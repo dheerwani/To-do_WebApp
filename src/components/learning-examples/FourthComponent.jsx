b20402b58e3872be1875ddd0b28f0c868cbf023b
Dheer <dheer.wani@iitb.org>
2021-05-22 23:11:37 +0530

    Started Building basics

---
+++ src/components/learning-examples/FourthComponent.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+export default class FourthComponent extends Component {
+    render(){
+      return (
+        <div className="fourthComponent">
+          Fourth Component
+        </div>
+      );
+    }
+  }
